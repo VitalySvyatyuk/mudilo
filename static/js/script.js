5f5ebf5c99f516a4e1b1c096c6d19adb070bbfb9
Vitaly Svyatyuk <goodgame1945@gmail.com>
2020-12-10 21:54:11 +0300

    update url

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 $('input[name="search"]').on('input', function(){
   $.ajax({
-    url: 'http://127.0.0.1:8000/api/v1/search' + '?search=' + this.value,
+    url: 'https://mudilo.me/api/v1/search' + '?search=' + this.value,
     type: 'GET',
     dataType: 'json',
     success: function(res) {
